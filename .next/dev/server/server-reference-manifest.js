ce0d9a4e7e4fe2ad769d8d58273a01901f5b80b2
naveen <ngowda759@gmail.com>
2026-06-26 08:47:16 +0000

    additional files

--- .next/dev/server/server-reference-manifest.js
+++ .next/dev/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"sAFTHjlkaL0By5YCor54kMENRLJk8M+tWrUvsp0nA2U=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"vwO6Bzub5Flem0lkQy9BrZrzMZW8VrUScdm5SvlhxBA=\"\n}"
