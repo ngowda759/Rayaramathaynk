25288851b81e649550aad4447ab25ff57327bda5
naveen <ngowda759@gmail.com>
2026-06-30 09:09:05 +0530

    Delete .next/dev directory

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"XGyV6inBaxqCdXNAKB3M2/q+EgOZinCvJBbOg/XhAlc=\"\n}"
--- .next/dev/static/chunks/app_layout_tsx_007e4b2._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__0bl8nle._.css","static/chunks/node_modules_next_dist_0-6zqf5._.js","static/chunks/node_modules_@firebase_auth_dist_esm_1_7j2ws._.js","static/chunks/node_modules_re2js_build_index_esm_1n4o7m-.js","static/chunks/node_modules_@firebase_firestore_dist_common-456515ba_esm_0elgku3.js","static/chunks/node_modules_@firebase_firestore_dist_index_esm_1av2adu.js","static/chunks/node_modules_@firebase_firestore_dist_common-456515ba_esm_0sytgnj.js","static/chunks/node_modules_@firebase_storage_dist_index_esm_009qcw2.js","static/chunks/node_modules_0v369_w._.js","static/chunks/_0bv1lpk._.js"],
-    source: "dynamic"
-});
