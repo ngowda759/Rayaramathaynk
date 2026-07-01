f659d710f40f77dcdfb342a359cb124968851059
naveen <ngowda759@gmail.com>
2026-07-01 07:55:40 +0000

    adding Social Media Sites

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -4,11 +4,11 @@ import TempleInfo from "@/components/home/TempleInfo";
 import TempleMap from "@/components/home/TempleMap";
 import FeaturedSevas from "@/components/home/FeaturedSevas";
 import TempleTimings from "@/components/home/TempleTimings";
-import QuickServices from "@/components/home/QuickServices";
 import AnnouncementBar from "@/components/home/AnnouncementBar";
 import UpcomingEvents from "@/components/home/UpcomingEvents";
 import GalleryPreview from "@/components/home/GalleryPreview";
 import DonationCTA from "@/components/home/DonationCTA";
+import SocialConnect from "@/components/home/SocialConnect";
 import Footer from "@/components/layout/Footer";
 import Panchanga from "@/components/home/Panchanga";
 
@@ -23,7 +23,7 @@ export default function Home() {
       <TempleMap />
       <FeaturedSevas />
       <TempleTimings />
-      <QuickServices />
+      <SocialConnect />
       <UpcomingEvents />
       <GalleryPreview />
       <DonationCTA />
